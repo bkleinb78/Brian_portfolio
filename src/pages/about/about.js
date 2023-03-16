18771fc5253842b658953c1577b5cb1c36905ef4
bkleinb78 <y2bk728@aol.com>
2023-03-16 18:31:10 -0500

    created the pages folder and added the about folder along with the about.js

---
+++ src/pages/about/about.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import Navbar from "../components/navbar";
+import Image1 from "../assets/image1.jpg";
+import Image2 from "../assets/image2.jpg";
+import "../App.css";
+
+export default function about() {
+  return (
+    <div className="App">
+      <div className="main-div">
+        <div className="navbar-top">
+          <Navbar />
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p className="header-title">ABOUT ME</p>
+            <br />
+            <p className="content-aboutme">
+              I believe that people should do things in which they are good at
+              or in which their heart lays. That's why I chose freelance web
+              development as my career because I believe I'm good at this and I
+              am always honest and I am never afraid of doing hard work. I
+              majored on Computer Science Engineering and I have taken many
+              lessons on modern web designing from famous institutions.I majored
+              on Computer Science Engineering and I have taken many lessons on
+              modern web designing from famous institutions.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <img src={Image2} className="about-image" />
+            <img src={Image1} className="about-image1" />
+            <div className="content-bio">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="number">44</p>
+                  <p className="number">31</p>
+                  <p className="number">175</p>
+                  <p className="number">1</p>
+                  <p className="number">1</p>
+                </div>
+                <div className="col-md-9">
+                  <p className="person-info">Years Old</p>
+                  <p className="person-info">Repositories</p>
+                  <p className="person-info">Contributions</p>
+                  <p className="person-info">Amazing Fiance</p>
+                  <p className="person-info">Year Developing</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
